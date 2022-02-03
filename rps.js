d8093aea0f0600045490fbb3a016bf5daa777dda
Yahya Ghoor <ghooryahya@gmail.com>
2022-02-03 06:46:12 +0200

    Added function to play a round of rps

--- rps.js
+++ rps.js
@@ -12,7 +12,30 @@ function computerPlay() {
 }
 
 function getPlayerChoice() {
-  let playerChoice = prompt("Please pick an object", "rock");
+  let playerChoice = prompt("Please pick a hand", "Paper");
 
   return playerChoice.toLowerCase();
+}
+
+function playRound() {
+  let result;
+  playerChoice = getPlayerChoice();
+  computerChoice = computerPlay();
+
+  if (playerChoice == "rock" && computerChoice == "scissors") {
+    console.log("You win! rock beats scissors") 
+    result = "player win";  
+  } else if (playerChoice == "paper" && computerChoice == "rock") {
+    console.log("You win! paper beats rock")   
+    result = "player win"; 
+  } else if (playerChoice == "scissors" && computerChoice == "paper") {
+    console.log("You win! scissors beats paper")
+    result = "player win";    
+  } else if (playerChoice == computerChoice) {
+    console.log("It's a draw!, no one wins")   
+    result = "draw"; 
+  } else {
+    console.log(`You lose! ${computerChoice} beats ${playerChoice}`)
+    result = "player loss";    
+  }
 }
